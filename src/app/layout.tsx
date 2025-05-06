85e57922d2b0b0e3b94adaf4e39ca18066ab73fe
Alexey Bondarenko <sky4tress@gmail.com>
2025-05-07 00:29:34 +0100

    feat: Add Google Analytics scripts for tracking user interactions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { i18n } from '@/i18n/settings';
 import './globals.css';
+import Script from 'next/script';
 
 export const metadata = {
   title: 'Logic',
@@ -13,6 +14,20 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang={i18n.defaultLocale}>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-HN93HGTBLX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HN93HGTBLX');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
